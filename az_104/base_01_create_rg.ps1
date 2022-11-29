4592cb9170db51e247869632169f4dc8da184c8f
Ravan Nannapaneni <Ravan.Nannapaneni@oliverwyman.com>
2022-11-29 15:16:27 +0800

    Renamed script ...

---
+++ az_104/base_01_create_rg.ps1
@@ -0,0 +1,12 @@
+$rgName = "test_prep_rg"
+$loc = "Southeast Asia"
+
+$t = Get-AzResourceGroup -Name $rgName  
+
+if($t -eq $null) {
+    echo "Resource group doesn't exist ... creating ... "
+    New-AzResourceGroup -Name $rgName -Location $loc
+} else { 
+    echo "Resource group exist ... skipping ... "
+}
+
